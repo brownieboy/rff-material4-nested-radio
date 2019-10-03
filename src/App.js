bc39156e2715ddc1f6c1de58b062ce48e5199c9b
Michael Brown <michaelbrown@temenos.com>
2019-10-03 14:12:44 +1000

    Radios mapped, back door data though

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Form, Field } from "react-final-form";
 import {
   FormControl,
@@ -11,63 +11,119 @@ import {
 
 import "./App.css";
 
+// Thin wrapper from final-form-material-ui lib
+const RadioWrapper = ({
+  input: { checked, value, name, onChange, ...restInput },
+  meta,
+  ...rest
+}) => (
+  <Radio
+    {...rest}
+    name={name}
+    inputProps={restInput}
+    onChange={onChange}
+    checked={checked}
+    value={value}
+  />
+);
+
+const allData = [
+  {
+    troupe: { value: "stooges", label: "The Three Stooges" },
+    members: [
+      { value: "larry", label: "Larry" },
+      { value: "moe", label: "Moe" },
+      { value: "curly", label: "Curly" }
+    ]
+  },
+  {
+    troupe: { value: "marx", label: "The Marx Brothers" },
+    members: [
+      { value: "chico", label: "Chico" },
+      { value: "harpo", label: "Harpo" },
+      { value: "groucho", label: "Groucho" },
+      { value: "zeppo", label: "Zeppo" }
+    ]
+  }
+];
+
 const onSubmit = async values => {
   const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
   await sleep(300);
   window.alert(JSON.stringify(values, 0, 2));
 };
 
-function App() {
-  return (
-    <div style={{margin: "40px"}}>
-      <Form
-        onSubmit={onSubmit}
-        initialValues={{ stooge: "larry" }}
-        render={({ handleSubmit, reset, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit} noValidate>
-            <FormControl component="fieldset">
-              <FormLabel component="legend">Best Stooge</FormLabel>
-              <RadioGroup row>
-                <FormControlLabel
-                  label="Larry"
-                  control={
-                    <Field
-                      name="stooge"
-                      component={Radio}
-                      type="radio"
-                      value="larry"
+class App extends Component {
+  renderItems = () => {
+    console.log("renderItems ->  props: ", this.props);
+  };
+
+  render() {
+    return (
+      <div style={{ margin: "40px" }}>
+        <Form
+          onSubmit={onSubmit}
+          initialValues={{ stooge: "larry" }}
+          values={allData}
+          render={({ handleSubmit, reset, submitting, pristine }) => (
+            <form onSubmit={handleSubmit} noValidate>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Funny groups:</FormLabel>
+                <RadioGroup>
+                  {allData.map(value => (
+                    <FormControlLabel
+                      label={value.troupe.label}
+                      control={
+                        <Field
+                          name="troupe"
+                          component={RadioWrapper}
+                          type="radio"
+                          value={value.troupe.value}
+                        />
+                      }
                     />
-                  }
-                />
-                <FormControlLabel
-                  label="Moe"
-                  control={
-                    <Field
-                      name="stooge"
-                      component={Radio}
-                      type="radio"
-                      value="moe"
-                    />
-                  }
-                />
-                <FormControlLabel
-                  label="Curly"
-                  control={
-                    <Field
-                      name="stooge"
-                      component={Radio}
-                      type="radio"
-                      value="curly"
-                    />
-                  }
-                />
-              </RadioGroup>
-            </FormControl>
-          </form>
-        )}
-      />
-    </div>
-  );
+                  ))}
+                  {/* <FormControlLabel
+                    label="Larry"
+                    control={
+                      <Field
+                        name="stooge"
+                        component={RadioWrapper}
+                        type="radio"
+                        value="larry"
+                      />
+                    }
+                  />
+                  <FormControlLabel
+                    label="Moe"
+                    control={
+                      <Field
+                        name="stooge"
+                        component={RadioWrapper}
+                        type="radio"
+                        value="moe"
+                      />
+                    }
+                  />
+                  <FormControlLabel
+                    label="Curly"
+                    control={
+                      <Field
+                        name="stooge"
+                        component={RadioWrapper}
+                        type="radio"
+                        value="curly"
+                      />
+                    }
+                  /> */}
+                </RadioGroup>
+              </FormControl>
+            </form>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
