aa9b09c0c3acf84cef6885265e359fb7b843f606
Michael Brown <michaelbrown@temenos.com>
2019-10-03 11:59:37 +1000

    Workiing with ffmui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Field } from "react-final-form";
+import {
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio
+} from "@material-ui/core";
+// import { Radio } from "final-form-material-ui";
+
+import "./App.css";
+
+const onSubmit = async values => {
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: "40px"}}>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{ stooge: "larry" }}
+        render={({ handleSubmit, reset, submitting, pristine, values }) => (
+          <form onSubmit={handleSubmit} noValidate>
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Best Stooge</FormLabel>
+              <RadioGroup row>
+                <FormControlLabel
+                  label="Larry"
+                  control={
+                    <Field
+                      name="stooge"
+                      component={Radio}
+                      type="radio"
+                      value="larry"
+                    />
+                  }
+                />
+                <FormControlLabel
+                  label="Moe"
+                  control={
+                    <Field
+                      name="stooge"
+                      component={Radio}
+                      type="radio"
+                      value="moe"
+                    />
+                  }
+                />
+                <FormControlLabel
+                  label="Curly"
+                  control={
+                    <Field
+                      name="stooge"
+                      component={Radio}
+                      type="radio"
+                      value="curly"
+                    />
+                  }
+                />
+              </RadioGroup>
+            </FormControl>
+          </form>
+        )}
+      />
     </div>
   );
 }
